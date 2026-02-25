4f92d823abb2453814944f567e3100fbf37683dc
adrien nana <kakonana7@gmail.com>
2026-02-25 04:17:51 +0100

    Building the Event Publisher

---
+++ common/src/main/kotlin/empire/digiprem/chirp/infra/message_queue/EventPublisher.kt
@@ -0,0 +1,29 @@
+package empire.digiprem.empire.digiprem.chirp.infra.message_queue
+
+import empire.digiprem.empire.digiprem.chirp.domain.ChirpEvent
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class EventPublisher(
+    private val rabbitTemplate: RabbitTemplate,
+) {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+
+
+    fun <T: ChirpEvent> publish(event:T){
+        try {
+            rabbitTemplate.convertAndSend(
+                event.exchange,
+                event.eventKey,
+                event
+            )
+            logger.info("successfully published event: ${event.eventId} ")
+        }catch (e:Exception){
+            logger.error("failed to publish ${event.eventKey} event", e)
+        }
+    }
+}
